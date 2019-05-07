12b97205174bec2ce470a8ba3afbf11a3cc1a670
Van Steven Nguyen <nvguyen@gmail.com>
2019-05-07 17:32:55 +0200

    views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,11 +4,15 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location])
+    @event = Event.create!(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location], admin_id: current_user.id)
     if @event.save
       redirect_to root_path
     else
       render "new"
     end
+  end
+
+  def show
+    @event = Event.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :attendances
 
-  root "events#index"
+  root "home#index"
   resources :events
 
   devise_for :users
