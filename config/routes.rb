56c533d7a6106a696afddae6bfd59009ee3b9a5c
Van Steven Nguyen <nvguyen@gmail.com>
2019-05-07 14:20:15 +0200

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :static_pages, only: [:index, :secret]
   resources :attendances
 
   root "events#index"
   resources :events
-  
+
   resources :users
   devise_for :users
+
+  resources :sessions
 end
